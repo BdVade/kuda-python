1548484e3e859bd5ab9503045399e668441b396f
Victor Aderibigbe <victoraderibigbe03@gmail.com>
2022-07-07 02:20:45 +0100

    XMLTOPEM conversion, encryption class structure
    Got XML to PEM conversion to work.

--- kuda/encryption.py
+++ kuda/encryption.py
@@ -1,19 +1,30 @@
 import rsa
+from .utils import XMLtoPEM
 
 
-class RSAEncryption:
+class BaseEncryption:
 
-    @classmethod
-    def encrypt(cls, public_key):
+    def __init__(self, private_key, public_key):
+        private_key = XMLtoPEM(private_key)
+    #     TODO: check how to get the private key from a rsakey object get it, and assign it to attributes(self.etc)
+
+    def encrypt(self,):
         pass
 
-    @classmethod
-    def decrypt(cls, private_key):
+    def decrypt(self,):
         pass
 
 
-class AES256Encryption:
+class RSAEncryption(BaseEncryption):
 
-    @classmethod
-    def encrypt(cls):
+    def encrypt(self,):
+        pass
+
+    def decrypt(self,):
+        pass
+
+
+class AES256Encryption(BaseEncryption):
+
+    def encrypt(self,):
         pass
--- kuda/utils.py
+++ kuda/utils.py
@@ -3,23 +3,28 @@ from Crypto.PublicKey.RSA import construct
 from base64 import b64decode
 
 
-def XMLtoPEM(private_key):
+def XMLtoPEM(private_key: str):
     xml = ET.fromstring(private_key)
     modulus = xml.find('Modulus').text
     exponent = xml.find('Exponent').text
+    private_exponent = xml.find('D').text
     p = xml.find('P').text
     q = xml.find('Q').text
     mod_b64 = b64decode(modulus.encode())
     exp_b64 = b64decode(exponent.encode())
+    p_exp_b64 = b64decode(private_exponent.encode())
     p_b64 = b64decode(p.encode())
     q_b64 = b64decode(q.encode())
     exp = ''.join(['{:02x}'.format(x) for x in exp_b64])
     mod = ''.join(['{:02x}'.format(x) for x in mod_b64])
+    p_exp = ''.join(['{:02x}'.format(x) for x in p_exp_b64])
     new_p = ''.join(['{:02x}'.format(x) for x in p_b64])
     new_q = ''.join(['{:02x}'.format(x) for x in q_b64])
     exp_num = int(exp, 16)
     mod_num = int(mod, 16)
+    p_exp_num = int(p_exp, 16)
 
-    return construct((mod_num, exp_num, exp_num), consistency_check=True)
+    return construct((mod_num, exp_num, p_exp_num), consistency_check=True)
 
 
+print(XMLtoPEM())
